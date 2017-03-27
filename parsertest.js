bf1da7edbf396855414f13128cbdf94fdb4cbddb
/u/leo60228 <iakornfeld@gmail.com>
2017-03-27 18:59:07 +0000

    wip

--- parsertest.js
+++ parsertest.js
@@ -1,41 +1,59 @@
+/*global parserstart*/
+
 let acorn = require('acorn');
 
 let typeMaps = {};
 
 acorn.plugins.BetterScript = function (parser) {
+  let walker = require('acorn/dist/walk');
   let tokens = [];
 
   parser.extend("readToken", function (nextMethod) {
     return function readToken(code, varDecl = false) {
-      if (acorn.isIdentifierStart(code, parser.options.ecmaVersion >= 6) || code === 92 /* '\' */) {
-        let word = parser.readWord1();
+      if (acorn.isIdentifierStart(code, this.options.ecmaVersion >= 6) || code === 92 /* '\' */) {
+        let word = this.readWord1();
         let type = acorn.tokTypes.name;
-        if (parser.keywords.test(word)) {
-          if (parser.containsEsc) {
-            parser.raiseRecoverable(parserstart, "Escape sequence in keyword " + word);
+        if (this.keywords.test(word)) {
+          if (this.containsEsc) {
+            this.raiseRecoverable(parserstart, "Escape sequence in keyword " + word);
           }
           type = acorn.keywordTypes[word];
         }
 
         if (tokens.length === 0) {
           tokens.push(word);
-          return parser.finishToken(type, 'let');
+          return this.finishToken(type, 'let');
         } else {
           tokens.push(word);
         };
 
         if (tokens.length === 2) {
-          typeMaps[parser.pos] = tokens[0];
+          typeMaps[this.pos] = tokens[0];
         }
 
-        return parser.finishToken(type, word);
+        return nextMethod.call(this, code);
       }
 
       tokens = [];
 
-      return parser.getTokenFromCode(code);
+      return nextMethod.call(this, code);
     }
-  })
+  });
+  
+  parser.extend('parse', function(nextMethod) {
+    return function() {
+      let program = nextMethod.call(this);
+      //console.log(program);
+      walker.simple(program, {
+        Indentifier: function(node) {
+          if (typeMaps.hasOwnProperty(node.end)) {
+            node.type = typeMaps[node.end];
+          }
+        }
+      });
+      return program;
+    }
+  });
 };
 
 console.log(acorn.parse('String.a; Number num = 1; String str = "abc"; function func(){}', {plugins: {BetterScript: true}}).body[1].declarations)
